2462aea5219de6b0cf7b47342569e3420f4eab8a
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-07-25 16:10:20 -0700

    fixed pycode style

--- 0x00-linear_algebra/1-trim_me_down.py
+++ 0x00-linear_algebra/1-trim_me_down.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []
-[the_middle.append(item[int(len(item) / 2) - 1: int(len(item) / 2) + 1]) for item in matrix]
+length = len(matrix[0])
+[the_middle.append(list(i[int(
+                        length / 2) - 1: int(
+                                         length / 2) + 1])) for i in matrix]
 print("The middle columns of the matrix are: {}".format(the_middle))
--- 0x00-linear_algebra/5-across_the_planes.py
+++ 0x00-linear_algebra/5-across_the_planes.py
@@ -10,6 +10,8 @@ def add_matrices2D(mat1, mat2):
     """
     returns matrix addition from two matrices
     """
-    mat1 = np.array(mat1)
-    mat2 = np.array(mat2)
-    return [list(item) for item in list(mat1 + mat2)] if mat1.shape == mat2.shape else None
+    m1 = np.array(mat1)
+    m2 = np.array(mat2)
+    return [list(
+            item) for item in list(
+                              m1 + m2)] if m1.shape == m2.shape else None
