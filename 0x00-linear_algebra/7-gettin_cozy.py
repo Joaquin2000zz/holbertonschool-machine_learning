686ffaacc0ed658bebfef9c8711e64af0a4bb36d
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-07-26 15:00:42 -0300

    concatenates two matrices along a specific axis

---
+++ 0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+module which contains cat_matrices2D
+"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """
+    cocatenates two 2D matrix
+    """
+
+    mat1copy = list(map(list, mat1))
+    if not axis:
+        for item in mat2: mat1copy.append(item.copy())
+        return mat1copy
+    elif axis == 1:
+        width = len(mat1copy[0]) if type(mat1copy) is list else None
+        n = len(mat2)
+        if width == n:
+            i = 0
+            for item in mat2:
+                for child in item:
+                    mat1copy[i] = mat1copy[i].copy()
+                    mat1copy[i].append(child); i += 1
+            return mat1copy
+        else:
+            return None
+    else:
+        return None
