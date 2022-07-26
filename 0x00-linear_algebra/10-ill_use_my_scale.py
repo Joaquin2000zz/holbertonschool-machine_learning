0d1fa3a363ae34be9f081a924a50b105e6c4393a
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-07-26 19:05:07 -0300

    calculates the shape of a numpy matrix using shape method from numpy arr

---
+++ 0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+contains np_shape and rec functions
+"""
+
+def np_shape(matrix):
+    """
+    calculates the shape of a numpy matrix using shape method from numpy arr
+    """
+    return matrix.shape
