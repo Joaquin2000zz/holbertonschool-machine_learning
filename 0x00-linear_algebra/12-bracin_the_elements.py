e43164c1d1a476e025d290b18b39da151594155c
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-07-26 19:28:56 -0300

    element wise operations between numpy matrices

---
+++ 0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+contains np_elementwise
+"""
+
+def np_elementwise(mat1, mat2):
+    """
+    element wise operations between numpy matrices
+    """
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
