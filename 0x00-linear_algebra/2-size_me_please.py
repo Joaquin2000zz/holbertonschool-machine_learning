b1bc54abe11bbd03298657d32909fd03d8d3ab31
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-07-26 12:01:42 -0300

    making& C:/Users/Joakote/AppData/Local/Programs/Python/Python37-32/python.exe c:/Users/Joakote/holbertonschool-machine_learning/0x00-linear_algebra/2-main.py

--- 0x00-linear_algebra/2-size_me_please.py
+++ 0x00-linear_algebra/2-size_me_please.py
@@ -3,11 +3,14 @@
 contains matrix_shape
 """
 
-import numpy as np
-
 
 def matrix_shape(matrix):
     """
-    calculates the shape of a matrix using shape method from numpy
+    calculates the shape of a matrix without using shape method from numpy
     """
-    return list(np.array(matrix).shape)
+
+    ls = []
+    while (type(matrix) is list):
+        ls.append(len(matrix))
+        matrix = matrix[0]
+        
