cda9e3e4ce57d2e997de8c373b4f089814c3f8c2
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-07-26 19:10:38 -0300

    transposes numpy matrix using transpose method from numpy arr

---
+++ 0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+contains np_transpose
+"""
+
+def np_transpose(matrix):
+    """
+    transposes numpy matrix using transpose method from numpy arr
+    """
+    return matrix.transpose()
