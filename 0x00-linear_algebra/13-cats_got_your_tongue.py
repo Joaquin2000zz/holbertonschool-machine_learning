9c8cc70469a56eae666d5fb2897e17c4f191e2e6
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-07-26 19:41:51 -0300

    concatenates two matrices along specific axis using concatenate function

---
+++ 0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+contains np_cat
+"""
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    concatenates two matrices along specific axis using concatenate function
+    """
+    return np.concatenate((mat1, mat2), int(axis))
+    
