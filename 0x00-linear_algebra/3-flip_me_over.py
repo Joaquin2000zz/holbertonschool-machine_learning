1bf680c0520fc364d589af833eb3cd39b4e5ccbd
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-07-26 12:36:24 -0300

    making the transpose numpy function instead using it

--- 0x00-linear_algebra/3-flip_me_over.py
+++ 0x00-linear_algebra/3-flip_me_over.py
@@ -4,9 +4,6 @@ contains matrix_transpose
 """
 
 
-from turtle import width
-
-
 def matrix_transpose(matrix):
     """
     returns the transpose of a 2D matrix without using transpose numpy method
