e38da541cb09ec83e29f2fea058227746c563c43
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-07-27 13:14:55 -0300

    fixed pycodestyle once again

--- math/0x00-linear_algebra/1-trim_me_down.py
+++ math/0x00-linear_algebra/1-trim_me_down.py
@@ -2,5 +2,5 @@
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []
 l = len(matrix[0])
-[the_middle.append(list(i[int(l / 2) - 1: int(l / 2) + 1])) for i in matrix]
+[the_middle.append(i[int(l / 2) - 1: int(l / 2) + 1]) for i in matrix]
 print("The middle columns of the matrix are: {}".format(the_middle))
