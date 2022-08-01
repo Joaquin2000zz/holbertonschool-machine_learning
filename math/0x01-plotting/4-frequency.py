8b54c86a8790795a468f9c7c17ceffe896e63fc3
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-08-01 15:57:37 -0300

    fixed plotting

---
+++ math/0x01-plotting/4-frequency.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+fig, ax = plt.subplots()
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title("Project A")
+ax.set_xlim(0, 100)
+ax.set_ylim(0, 30)
+ax.hist(student_grades, edgecolor="black", bins=range(0, 101, 10))
+plt.show()
