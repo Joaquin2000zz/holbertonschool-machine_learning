0ac7f9c7c4b85ca5e54433099d2218df3afc117c
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-08-03 16:18:44 -0300

    sigma notation with 1=0 to n with i^2

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+contains and summation_i_squared function
+"""
+
+
+def summation_i_squared(n):
+    """
+    sigma notation with 1=0 to n with i^2
+    returns none if values aren't numeric
+    recursion needed to don't use any loop
+    as the task says
+    """
+    flag = str(n)
+    if flag.replace('.', '', 1).isnumeric() is False:
+        return None
+    print(n)
+    if n == 0:
+        return 0
+    return summation_i_squared(n - 1) + n ** 2
