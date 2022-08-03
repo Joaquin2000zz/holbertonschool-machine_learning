54212969a5baa6cd2758a30ae2da55e58094c36c
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-08-03 16:19:29 -0300

    poly is a list of coefficients representing a polynomial

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,19 @@
+"""
+module which contains poly_derivative
+"""
+
+
+def poly_derivative(poly):
+    """
+    poly is a list of coefficients representing a polynomial
+    the index of the list represents the power of x that the coefficient belongs to
+    Example: if f(x) = x^3 + 3x + 5, poly is equal to [5, 3, 0, 1]
+    """
+    i = 0
+    ret = []
+    for mono in poly:
+
+        if i > 0:
+            ret.append(i * mono)
+        i += 1
+    return ret
