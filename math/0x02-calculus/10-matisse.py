72a0afbf163f6a2ca667b153c32e9e63cbac64dd
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-08-03 16:40:02 -0300

    fixed pycodestyle

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -6,7 +6,8 @@ module which contains poly_derivative
 def poly_derivative(poly):
     """
     poly is a list of coefficients representing a polynomial
-    the index of the list represents the power of x that the coefficient belongs to
+    the index of the list represents the power of x that the coefficient
+    belongs to
     Example: if f(x) = x^3 + 3x + 5, poly is equal to [5, 3, 0, 1]
     """
     i = 0
