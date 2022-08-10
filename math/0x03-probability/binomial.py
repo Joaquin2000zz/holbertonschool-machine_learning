c0631e3b0cebd7eadf8bd5b6aef754914d3d53a9
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-08-09 21:06:50 -0300

    class Binomial that represents a binomial distribution

---
+++ math/0x03-probability/binomial.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+module which contains the Binomial class
+"""
+
+
+class Binomial():
+    """
+    represents a binomial distribution
+    """
+
+    def __init__(self, data=None, n=1, p=0.5):
+        """
+        initialize a binomial distribution
+        """
+        if data or data is not None:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            res = 0
+            self.n = int(len(data) / 2)
+            for value in data:
+                if not isinstance(value, int) and not isinstance(value, float):
+                    raise ValueError('list values must be integers or floats')
+                res += value
+            self.p = float(self.reduce(res / self.n))
+        else:
+            if n > 0:
+                self.n = int(n)
+            else:
+                raise ValueError('n must be a positive value')
+            if p < 0 or p > 1:
+                raise ValueError('p must be greater than 0 and less than 1')
+            self.p = float(p)
+
+    def reduce(self, n):
+        """
+        function to reduce numbers to values
+        between 0 and 1
+        """
+        if n < 0:
+            return None
+        if n <= 1:
+            return n
+        ret = self.reduce(n / 10)
+
+        return ret
