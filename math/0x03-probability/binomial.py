209d0c40dfed1cb25b595c2f6b396faa3ebc376a
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-08-10 18:44:45 -0300

    casting to float

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -9,48 +9,48 @@ class Binomial():
     represents a binomial distribution
     """
 
+class Binomial:
+    """class Binomial that represents a binomial distribution"""
+
     def __init__(self, data=None, n=1, p=0.5):
-        """
-        initialize a binomial distribution
-        """
+        """Class contructor"""
         if data or data is not None:
-            if not isinstance(data, list):
-                raise TypeError('data must be a list')
+            if type(data) is not list:
+                raise TypeError("data must be a list")
             if len(data) < 2:
-                raise ValueError('data must contain multiple values')
-            res = 0
+                raise ValueError("data must contain multiple values")
+            aux = 0
             i = 0
-            for value in data:
-                if not isinstance(value, int) and not isinstance(value, float):
-                    raise ValueError('list values must be integers or floats')
-                res += value
+            aux2 = 0
+            for item in data:
+                aux += item
                 i += 1
-            mu = res / i
-            variation = 0
-            for x in data:
-                variation += (x - mu) ** 2
-            variation /= i
-            self.p = float(1 - (variation / mu))
-            self.n = round(mu / self.p)
-            self.p = float(mu / self.n)
+            m = aux / i
+            for item2 in data:
+                aux2 += (item2 - m) ** 2
+            x = aux2 / i
+            self.p = float(1 - (x / m))
+            self.n = round(m / self.p)
+            self.p = float(m / self.n)
         else:
-            if n > 0:
-                self.n = n
-            else:
-                raise ValueError('n must be a positive value')
+            if n <= 0:
+                raise ValueError("n must be a positive value")
             if p <= 0 or p >= 1:
-                raise ValueError('p must be greater than 0 and less than 1')
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = n
             self.p = p
 
-    def pmf(self, k):
-        """Calculates the value of the PMF for a given number of successes"""
-        if type(k) is not int:
+def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of “successes”
+        """
+        if not isinstance(k, int):
             k = int(k)
         if k < 0 or k > self.n:
             return 0
-        com = self.factorial(self.n) / (self.factorial
-                                        (self.n - k) * self.factorial(k))
-        return (com * (self.p ** k) * ((1 - self.p) ** (self.n - k)))
+        nFact = self.factorial(self.n)
+        comb = nFact / (self.factorial(self.n - k) * self.factorial(k))
+        return comb * ((self.p ** k) * (1 - self.p) ** (self.n - k))
 
     def cdf(self, k):
         """
