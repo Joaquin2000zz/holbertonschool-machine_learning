2c83eff851f1e8b8f9172c33821902eab4c247b3
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-08-20 11:49:24 -0300

    added first model of neural network

---
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+module which contains NeuralNetwork
+"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    defines a neural network with one hidden layer
+    performing binary classification
+    """
+    def __init__(self, nx, nodes):
+        """
+        NeuralNetwork constructor
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.W1 = np.random.randn(3, nx)
+        self.b1 = np.zeros(shape=(nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
