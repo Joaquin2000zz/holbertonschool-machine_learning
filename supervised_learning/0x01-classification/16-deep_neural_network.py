5e49d4d6f5339c8be49d8729a131047f022bc0cb
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-08-21 18:44:52 -0300

    done Deep Neural Network init using he-et-al method to the weight values

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -18,20 +18,20 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or not layers:
+        if not isinstance(layers, list):
             raise TypeError("layers must be a list of positive integers")
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
-        for i in range(1, self.L + 1):
+        for i in range(0, self.L):
             if layers[i - 1] < 1:
                 raise TypeError("layers must be a list of positive integers")
-            self.weights[f'b{i}'] = np.zeros(shape=(layers[i - 1], 1))
-            if i - 2 > -1:
-                he_et_al = np.sqrt(2 / layers[i - 2])
-                Wn = np.random.randn(layers[i - 1], layers[i - 2]) * he_et_al
-                self.weights[f'W{i}'] = Wn
+            self.weights[f'b{i + 1}'] = np.zeros(shape=(layers[i], 1))
+            if i - 1 > -1:
+                he_et_al = np.sqrt(2 / layers[i - 1])
+                Wn = np.random.randn(layers[i], layers[i - 1]) * he_et_al
+                self.weights[f'W{i + 1}'] = Wn
             else:
                 he_et_al = np.sqrt(2 / nx)
-                Wn = np.random.randn(layers[i - 1], nx) * he_et_al
-                self.weights[f'W{i}'] = Wn
+                Wn = np.random.randn(layers[i], nx) * he_et_al
+                self.weights[f'W{i + 1}'] = Wn
