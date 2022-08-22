05be68c01a78f62ff4cb45d3d64f83bcd5d6fcc0
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-08-21 23:20:34 -0300

    implemented evaluate predictions performing binary classification

--- supervised_learning/0x01-classification/20-deep_neural_network.py
+++ supervised_learning/0x01-classification/20-deep_neural_network.py
@@ -86,4 +86,4 @@ class DeepNeuralNetwork:
         Evaluates neuron’s predictions performing binary classification
         """
         _, A = self.forward_prop(X)
-        return np.where(A >= 0.5, 1, 0), self.cost(Y, A)
+        return np.where(A.get(f'A{self.__L}') >= 0.5, 1, 0), self.cost(Y, A.get(f'A{self.__L}'))
