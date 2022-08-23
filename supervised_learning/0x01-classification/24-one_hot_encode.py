fedc3e4b0c440769827fca3e38e3b776029dff18
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-08-22 21:58:43 -0300

    ended one hot encode successfully

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -10,6 +10,8 @@ def one_hot_encode(Y, classes):
     """
     converts a numeric label vector into a one-hot matrix
     """
+    if Y.shape == 0 or classes < 1:
+        return None
     M = np.zeros(shape=(Y.shape[0], classes))
     for i in range(classes):
         M[Y[i]][i] = 1
