returning the derivative instead the derivative corrected

supervised_learning/0x03-optimization/9-Adam.py
@@ -21,9 +21,9 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
              and the new second moment, respectively
     """
     Vd = (beta1 * v) + ((1 - beta1) * grad)
-    Vd /= ((1 - (beta1 ** t)) + epsilon)
+    Vdc = Vd / ((1 - (beta1 ** t)) + epsilon)
 
     Sd = (beta2 * s) + (1 - beta2) * (grad ** 2)
-    Sd /= ((1 - (beta2 ** t)) + epsilon)
+    Sdc = Vd / ((1 - (beta2 ** t)) + epsilon)
 
     return var - alpha * (Vd / (np.sqrt(Sd) + epsilon)), Vd, Sd