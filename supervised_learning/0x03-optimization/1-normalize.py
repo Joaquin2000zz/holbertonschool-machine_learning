d176ed7669fb2474dc4d701fb783611d911c9d72
Joaquin Victoria <3942@holbertonstudents.com>
2022-08-31 18:28:13 -0300

    fixed pycodestyle

--- supervised_learning/0x03-optimization/1-normalize.py
+++ supervised_learning/0x03-optimization/1-normalize.py
@@ -13,7 +13,8 @@ def normalize(X, m, s):
         - d is the number of data points
         - nx is the number of features
     * m is a numpy.ndarray of shape (nx,) that contains the mean of all features of X
-    * s is a numpy.ndarray of shape (nx,) that contains the standard deviation of all features of X
+    * s is a numpy.ndarray of shape (nx,) that contains the standard deviation
+      of all features of X
     Returns: The normalized X matrix
     """
     return (X - m) / s
