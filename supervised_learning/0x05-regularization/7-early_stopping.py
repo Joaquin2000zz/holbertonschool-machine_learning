8ec7b387e0b750c634c2e9556321640aec04563a
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-09-08 18:16:53 -0300

    fixed pycodestyle

--- supervised_learning/0x05-regularization/7-early_stopping.py
+++ supervised_learning/0x05-regularization/7-early_stopping.py
@@ -20,7 +20,7 @@ def early_stopping(cost, opt_cost, threshold, patience, count):
     Returns: a boolean of whether the network should be stopped early,
              followed by the updated count
     """
-    if opt_cost- cost <= threshold:
+    if opt_cost - cost <= threshold:
         count += 1
     else:
         count = 0
