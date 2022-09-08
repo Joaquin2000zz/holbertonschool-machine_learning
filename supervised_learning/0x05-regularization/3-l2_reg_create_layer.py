7e2149e84767b9aafbec71a494d2c4287949ceb8
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-09-07 21:49:36 -0300

    using layers of tf instead tf.keras

---
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+module which contains l2_reg_create_layer function
+"""
+import tensorflow.compat.v1 as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """
+    Creates a tensorflow layer that includes L2 regularization:
+
+    * prev is a tensor containing the output of the previous layer
+    * n is the number of nodes the new layer should contain
+    * activation is the activation function that should be used on the layer
+    * lambtha is the L2 regularization parameter
+    Returns: the output of the new layer
+    """
+    het_et_al = tf.keras.initializers.VarianceScaling(scale=2.0,
+                                                      mode=("fan_avg"))
+    L2 = tf.keras.regularizers.L2(lambtha)
+    layer = tf.layers.Dense(n, activation=activation,
+                                  kernel_initializer=het_et_al,
+                                  kernel_regularizer=L2)
+    return layer(prev)
