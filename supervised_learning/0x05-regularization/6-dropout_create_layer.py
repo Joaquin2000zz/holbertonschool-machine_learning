488687d6b8921962faafff148838a1e819c616df
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-09-08 10:21:07 -0300

    ended successfully dropout layer

---
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+module which contains l2_reg_create_layer function
+"""
+import tensorflow.compat.v1 as tf
+
+
+def dropout_create_layer(prev, n, activation, keep_prob):
+    """
+    creates a layer of a neural network using dropout:
+
+    * prev is a tensor containing the output of the previous layer
+    * n is the number of nodes the new layer should contain
+    * activation is the activation function that should be used on the layer
+    * keep_prob is the probability that a node will be kept
+    Returns: the output of the new layer
+    """
+    het_et_al = tf.keras.initializers.VarianceScaling(scale=2.0,
+                                                      mode=("fan_avg"))
+    layer = tf.layers.Dense(n,
+                            activation=activation,
+                            kernel_initializer=het_et_al)
+    x = layer(prev)
+
+    return tf.layers.Dropout(rate=keep_prob)(x)
