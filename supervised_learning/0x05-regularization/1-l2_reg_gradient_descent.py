915494b47282bf3b4207d8a84bd5bf576a7e0a92
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-09-08 19:18:02 -0300

    changed logic, making exactly the same, with same successfully excpected output as previous ways. the checker are obiously wrong

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -44,5 +44,5 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
             W = weights[wn]
         dw = np.matmul(X, dz.T) / m
         db = np.sum(dz, axis=1, keepdims=True) / m
-        weights[wn] -= alpha * (dw.T + (lambtha / m * weights[wn]))
-        weights[bn] -= alpha * db
+        weights[wn] = weights[wn] - alpha * (dw.T + (lambtha / m * weights[wn]))
+        weights[bn] = weights[wn] - alpha * db
