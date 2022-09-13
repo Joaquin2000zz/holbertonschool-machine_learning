f76f4cc9f2b1b78cc54bea6f0b3dfa8a18b2634b
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-09-13 12:51:06 -0300

    fixed pycodestyle and using string that specifies the loss instead pass tensor

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -16,9 +16,9 @@ def optimize_model(network, alpha, beta1, beta2):
     * beta2 is the second Adam optimization parameter
     Returns: None
     """
-    optimizer = K.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2)
-    loss = K.losses.CategoricalCrossentropy()
+    optimizer = K.optimizers.Adam(learning_rate=alpha,
+                                  beta_1=beta1, beta_2=beta2)
     network.compile(optimizer=optimizer,
-                    loss=loss,
+                    loss='categorical_crossentropy',
                     metrics=["accuracy"])
     return None
