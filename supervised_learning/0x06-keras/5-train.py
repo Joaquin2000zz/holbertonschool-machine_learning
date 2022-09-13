f9c152190bdea4a4c093bf16b2f79d8e533a2ba0
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-09-13 13:23:34 -0300

    same as 4 but now can use validation data

---
+++ supervised_learning/0x06-keras/5-train.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+module which contains
+"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """
+    Based on 4-train, updates the function to also analyze validaiton data:
+
+    * validation_data is the data to validate the model with, if not None
+    """
+    return network.fit(x=data, y=labels, epochs=epochs,
+                       batch_size=batch_size, verbose=verbose,
+                       shuffle=shuffle, validation_data=validation_data)
