7c4f7da470bd10fdce79bde11bad3903afae4eac
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-09-13 13:56:23 -0300

    same as 5 but early stopping callback implemented

---
+++ supervised_learning/0x06-keras/6-train.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+module which contains train_model
+"""
+from gc import callbacks
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                verbose=True, shuffle=False):
+    """
+    Based on 5-train, updates the function to also train
+    the model using early stopping:
+
+    * early_stopping is a boolean that indicates whether early
+      stopping should be used
+        - early stopping should only be performed if validation_data exists
+        - early stopping should be based on validation loss
+    * patience is the patience used for early stopping
+    """
+    if early_stopping:
+        early_stopping = K.callbacks.EarlyStopping(patience=patience)
+    return network.fit(x=data, y=labels, epochs=epochs,
+                       batch_size=batch_size, verbose=verbose,
+                       shuffle=shuffle, validation_data=validation_data,
+                       callbacks=[early_stopping])
