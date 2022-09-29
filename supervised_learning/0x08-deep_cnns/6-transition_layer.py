c768f5543a7f24047ffccab9bd236483b721e1e4
Joaquin Victoria <3942@holbertonstudents.com>
2022-09-29 16:12:44 -0300

    when somthing works, don't touch it

--- supervised_learning/0x08-deep_cnns/6-transition_layer.py
+++ supervised_learning/0x08-deep_cnns/6-transition_layer.py
@@ -27,6 +27,7 @@ def transition_layer(X, nb_filters, compression):
     X = K.layers.Conv2D(nb_filters * compression, kernel_size=(1, 1),
                         kernel_initializer=het_et_al,
                         strides=(1, 1), padding='same')(X)
-    X = K.layers.AvgPool2D(2, strides=(2, 2), padding="valid")(X)
+    X = K.layers.AvgPool2D(pool_size=(2, 2), strides=(2, 2),
+                           padding="valid")(X)
 
     return X, nb_filters * compression
