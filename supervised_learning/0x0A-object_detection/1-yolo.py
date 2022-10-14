49ae80f8ea5a90630589a3e461c840e3c80832aa
Joaquin Victoria <3942@holbertonstudents.com>
2022-10-14 18:55:08 -0300

    changed logic according the discussed on peer learning day

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -84,60 +84,53 @@ class Yolo:
         box_confidences = []
         box_class_probs = []
 
-        # obtaining input shape to make the calculation needed to the boxes
-        # for more detail https://www.youtube.com/watch?v=vRqSO6RsptU
-        _, input_width, input_height, _ = self.model.input.shape
-
         for i, output in enumerate(outputs):
 
             # normalizing values between 0 and 1 as the sigmoid function does
             confidence = self.__sigmoid(output[..., 4])
-            box_confidences.append(np.expand_dims(confidence), axis=-1)
+            box_confidences.append(np.expand_dims(confidence, axis=-1))
             box_class_probs.append(self.__sigmoid(output[..., 5:]))
 
-            # isolating t_x t_y t_w t_h from last dimention of the output
-            box = output[..., :4]
+            # obtaining t_x t_y t_w t_h
+            t_xy = output[..., :2]
+            t_wh = output[..., 2:4]
 
-            # obtaining grid shape and anchor boxes from output
-            grid_height, grid_width, anchors = output.shape[:3]
+            # corresponding anchor box
+            anchors = self.anchors[i]
 
-            # getting each offset of cells
-            c_x = np.arange(grid_width).reshape(1, grid_width)
-            c_x = np.repeat(c_x, grid_height, axis=0)
-            c_x = np.repeat(c_x[..., np.newaxis], anchors, axis=2)
-            c_y = np.arange(grid_width).reshape(1, grid_width)
-            c_y = np.repeat(c_y, grid_height, axis=0).T
-            c_y = np.repeat(c_y[..., np.newaxis], anchors, axis=2)
+            # getting grid width and height: number of grid cells
+            g_h, g_w = output.shape[:2]
 
-            # obtaining t_x t_y t_w t_h individualy
-            t_x = box[..., 0]
-            t_y = box[..., 1]
-            t_w = box[..., 2]
-            t_h = box[..., 3]
-
-            # width and height of its corresponding anchor box
-            p_w, p_h = self.anchors[i, :, 0], self.anchors[i, :, 1]
+            # grid cell indices
+            grid = np.tile(np.indices((g_h, g_w)).T, 3)
+            grid = grid.reshape((g_w, g_h) + anchors.shape)
 
             # given the extracted values, let's start with the calculations
             # for more information https://www.youtube.com/watch?v=vRqSO6RsptU
 
-            # bounding boxes
-            b_x = (self.__sigmoid(t_x) + c_x) / input_width
-            b_y = (self.__sigmoid(t_y) + c_y) / input_height
-            b_w = (p_w * np.exp(t_w)) / input_width
-            b_h = (p_h * np.exp(t_h)) / input_height
+            # computing center of each bounding box
+            b_xy = self.__sigmoid(t_xy) + grid
 
-            # adjunsting for CV2 (top left and bottom right point of image)
-            x_1 = (b_x - (b_w / 2)) * image_size[1]
-            y_1 = (b_y - (b_h / 2)) * image_size[0]
-            x_2 = (b_x + (b_w / 2)) * image_size[1]
-            y_2 = (b_y + (b_h / 2)) * image_size[0]
+            # computing width and height of each bounding box
+            b_wh = t_wh * np.exp(t_wh)
 
-            # setting values in its corresponding places
-            box[..., 0] = x_1
-            box[..., 1] = y_1
-            box[..., 2] = x_2
-            box[..., 3] = y_2
+            # normalizing b_xy and b_wh
+            # b_xy: divided by grid's size
+            # b_wh: divided by model's input shape
+            b_xy /= [g_h, g_w]
+            b_wh /= self.model.inputs[0].shape.as_list()[1:3]
+
+            # convert box center and weight in top left
+            # and bottom right corners
+            # top left
+            b_xy1 = b_xy - (b_wh / 2)
+            # bottom right
+            b_xy2 = b_xy + (b_wh / 2)
+
+            box = np.concatenate((b_xy1, b_xy2), axis=-1)
+
+            # multiplying by the original image size
+            box = box * np.tile(np.flip(image_size, axis=0), 2)
 
             boxes.append(box)
 
