3b9263479fc44cb3391fecb42f87560d95d8fb8a
Joaquin Victoria <3942@holbertonstudents.com>
2022-11-16 17:47:41 -0300

    using simple quotes instead duble

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-"""
+'''
 module which contain initialize function
-"""
+'''
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
 
 
 def initialize(X, k):
-    """
+    '''
     initializes variables for a Gaussian Mixture Model:
+
     - X is a numpy.ndarray of shape (n, d) containing the data set
     - k is a positive integer containing the number of clusters
     - You are not allowed to use any loops
@@ -19,7 +20,7 @@ def initialize(X, k):
         means for each cluster, initialized with K-means
       - S is a numpy.ndarray of shape (k, d, d) containing the covariance
         matrices for each cluster, initialized as identity matrices
-    """
+    '''
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
     if not isinstance(k, int) or k < 1:
