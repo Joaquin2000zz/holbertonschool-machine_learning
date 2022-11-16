9d3fed6a74396b50d932e271729543150d23be2b
Joaquin Victoria <3942@holbertonstudents.com>
2022-11-16 17:49:37 -0300

    just to check if the problem is with me or what

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -21,18 +21,18 @@ def initialize(X, k):
       - S is a numpy.ndarray of shape (k, d, d) containing the covariance
         matrices for each cluster, initialized as identity matrices
     '''
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None, None
-    if not isinstance(k, int) or k < 1:
-        return None, None, None
-    # sum of prior = 1
-    pi = np.full((k,), 1 / k)
 
-    # obtaining centroids means for each cluster
+    if type(k) is not int or k < 1:
+        return None, None, None
+
+    n, d = X.shape
+
+    pi = np.ones(shape=(k,)) * (1 / k)
+
     m, _ = kmeans(X, k)
 
-    _, d = X.shape
-    # creating S initialized as identity matrices
-    S = np.tile(np.identity(d), (k, 1, 1))
+    S = np.tile(np.identity(d), (k, 1)).reshape(k, d, d)
 
     return pi, m, S
