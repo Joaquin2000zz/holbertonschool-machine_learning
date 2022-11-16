796775fd60ed0fd75606ece34fe9b73b5118cdbf
Joaquin Victoria <3942@holbertonstudents.com>
2022-11-16 17:20:32 -0300

    just to test the requirement check

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-aadasd
+module which contain initialize function
 """
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
@@ -8,11 +8,23 @@ kmeans = __import__('1-kmeans').kmeans
 
 def initialize(X, k):
     """
-    asdasdasdasdasda
+    initializes variables for a Gaussian Mixture Model:
+
+    - X is a numpy.ndarray of shape (n, d) containing the data set
+    - k is a positive integer containing the number of clusters
+    - You are not allowed to use any loops
+    Returns: pi, m, S, or None, None, None on failure
+      - pi is a numpy.ndarray of shape (k,) containing the priors
+        for each cluster, initialized evenly
+      - m is a numpy.ndarray of shape (k, d) containing the centroid
+        means for each cluster, initialized with K-means
+      - S is a numpy.ndarray of shape (k, d, d) containing the covariance
+        matrices for each cluster, initialized as identity matrices
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None, None
-    if not isinstance(k, int) or k < 1:
+
+    if type(k) is not int or k < 1:
         return None, None, None
     # sum of prior = 1
     pi = np.full((k,), 1 / k)
