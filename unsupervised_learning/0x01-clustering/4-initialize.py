a391b889a75714bdf1e4439eb5bcdafe2cc11388
Joaquin Victoria <3942@holbertonstudents.com>
2022-11-16 17:41:08 -0300

    ???????

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -26,13 +26,13 @@ def initialize(X, k):
     if not isinstance(k, int) or k < 1:
         return None, None, None
     # sum of prior = 1
-    pi = np.ones(shape=(k,)) * 1 / k
+    pi = np.full((k,), 1 / k)
 
     # obtaining centroids means for each cluster
     m, _ = kmeans(X, k)
 
     _, d = X.shape
     # creating S initialized as identity matrices
-    S = np.tile(np.identity(d), (k, 1, 1))
+    S = np.tile(np.identity(d), (k, 1)).reshape(k, d, d)
 
     return pi, m, S
