d7bd0d0a4afd8399f64e239c6525660dd543f817
Joaquin Victoria <3942@holbertonstudents.com>
2022-12-01 22:15:09 -0300

    just to check if the checker hates me or wtf

--- unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
@@ -5,14 +5,12 @@ module which contains BayesianOptimization's class
 import numpy as np
 from scipy.stats import norm
 GP = __import__('2-gp').GaussianProcess
-
-
 class BayesianOptimization:
     """
     performs Bayesian optimization on a noiseless 1D Gaussian process
     """
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
-                 sigma_f=1, xsi=0.01, minimize=True):
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
         """
         f is the black-box function to be optimized
         - X_init is a numpy.ndarray of shape (t, 1)
@@ -41,6 +39,9 @@ class BayesianOptimization:
         self.f = f
 
         self.gp = GP(X_init, Y_init, l, sigma_f)
+        low, high = bounds
+        self.X_s = np.linspace(start=low, stop=high,
+                               num=ac_samples).reshape(-1, 1)
 
         X_s = np.linspace(bounds[0], bounds[1], num=ac_samples)
         self.X_s = X_s.reshape(-1, 1)
@@ -60,7 +61,6 @@ class BayesianOptimization:
           containing the expected improvement of each potential sample
         """
         mu, sigma = self.gp.predict(self.X_s)
-
         if self.minimize is True:
             Y_sample = np.min(self.gp.Y)
             im = Y_sample - mu - self.xsi
@@ -68,15 +68,13 @@ class BayesianOptimization:
             Y_sample = np.max(self.gp.Y)
             im = mu - Y_sample - self.xsi
 
-        ss = sigma.shape[0]
-        Z = np.zeros(ss)
-        for i in range(ss):
+        N = sigma.shape[0]
+        Z = np.zeros(N)
+        for i in range(N):
             if sigma[i] > 0:
                 Z[i] = im[i] / sigma[i]
             else:
                 Z[i] = 0
-            expo = im * norm.cdf(Z) + sigma * norm.pdf(Z)
-
-        X_next = self.X_s[np.argmax(expo)]
-
-        return X_next, expo
+            EI = im * norm.cdf(Z) + sigma * norm.pdf(Z)
+        X_next = self.X_s[np.argmax(EI)]
+        return X_next, EI
