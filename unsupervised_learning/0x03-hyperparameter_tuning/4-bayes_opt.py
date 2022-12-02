c9de2c3388156316217990a80fe006f0002abd1b
Joaquin Victoria <3942@holbertonstudents.com>
2022-12-01 22:33:07 -0300

    okay im done

--- unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
@@ -1,54 +1,71 @@
 #!/usr/bin/env python3
-'''
-Modulus that perfors bayesian optimization
-'''
+"""
+module which contains BayesianOptimization's class
+"""
 import numpy as np
 from scipy.stats import norm
 GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
-    '''
-    Class that performs bayesian optimization
-    '''
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
-                 sigma_f=1, xsi=0.01, minimize=True):
-        '''
-        Function init for bayesian optimization
-        '''
+    """
+    performs Bayesian optimization on a noiseless 1D Gaussian process
+    """
+
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
+        """
+        f is the black-box function to be optimized
+        - X_init is a numpy.ndarray of shape (t, 1)
+          representing the inputs already sampled with the black-box function
+        - Y_init is a numpy.ndarray of shape (t, 1)
+          representing black-box function's outputs for each input in X_init
+          * t is the number of initial samples
+        - bounds is a tuple of (min, max) representing the bounds
+          of the space in which to look for the optimal point
+        - ac_samples is the number of samples that
+          should be analyzed during acquisition
+        - l is the length parameter for the kernel
+        - sigma_f is the standard deviation given to the output
+          of the black-box function
+        - xsi is the exploration-exploitation factor for acquisition
+        - minimize is a bool determining whether optimization should
+          be performed for minimization (True) or maximization (False)
+        - Sets the following public instance attributes:
+          * f: the black-box function
+          * gp: an instance of the class GaussianProcess
+          * X_s: a numpy.ndarray of shape (ac_samples, 1) containing all
+            acquisition sample points, evenly spaced between min and max
+          * xsi: the exploration-exploitation factor
+          * minimize: a bool for minimization versus maximization
+        """
         self.f = f
-
         self.gp = GP(X_init, Y_init, l, sigma_f)
-
-        X_s = np.linspace(bounds[0], bounds[1], num=ac_samples)
-        self.X_s = X_s.reshape(-1, 1)
-
+        low, high = bounds
+        self.X_s = np.linspace(start=low, stop=high,
+                               num=ac_samples)[np.newaxis].T
         self.xsi = xsi
-
         self.minimize = minimize
 
     def acquisition(self):
-        '''
-        Function that calculates the next best sample location
-        '''
+        """
+        - calculates the next best sample location:
+        - Uses the Expected Improvement acquisition function
+        Returns: X_next, EI
+        - X_next is a numpy.ndarray of shape (1,)
+          representing the next best sample point
+        - EI is a numpy.ndarray of shape (ac_samples,)
+          containing the expected improvement of each potential sample
+        """
         mu, sigma = self.gp.predict(self.X_s)
 
-        if self.minimize is True:
-            Y_sample = np.min(self.gp.Y)
-            im = Y_sample - mu - self.xsi
-        else:
-            Y_sample = np.max(self.gp.Y)
-            im = mu - Y_sample - self.xsi
+        sample = self.gp.Y.min() if self.minimize else self.gp.Y.max()
 
-        ss = sigma.shape[0]
-        Z = np.zeros(ss)
-        for i in range(ss):
-            if sigma[i] > 0:
-                Z[i] = im[i] / sigma[i]
-            else:
-                Z[i] = 0
-            expo = im * norm.cdf(Z) + sigma * norm.pdf(Z)
-
-        X_next = self.X_s[np.argmax(expo)]
-
-        return X_next, expo
+        with np.errstate(divide='warn'):
+            imp = sample - mu - self.xsi if self.minimize else \
+                mu - sample - self.xsi
+            Z = imp / sigma
+            EI = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+            EI[sigma == .0] = .0
+        X_next = self.X_s[EI.argmax()]
+        return X_next, EI
