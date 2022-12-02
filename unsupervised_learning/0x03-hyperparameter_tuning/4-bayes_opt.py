7fb25ace7dbc65b6becfe810dcddc583b0c26665
Joaquin Victoria <3942@holbertonstudents.com>
2022-12-01 22:01:21 -0300

    just to check if the checker hates me or wtf

--- unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
@@ -1,50 +1,26 @@
 #!/usr/bin/env python3
-"""
-module which contains BayesianOptimization's class
-"""
+'''
+Modulus that perfors bayesian optimization
+'''
 import numpy as np
 from scipy.stats import norm
 GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
-    """
-    performs Bayesian optimization on a noiseless 1D Gaussian process
-    """
-
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
-                 l=1, sigma_f=1, xsi=0.01, minimize=True):
-        """
-        f is the black-box function to be optimized
-        - X_init is a numpy.ndarray of shape (t, 1)
-          representing the inputs already sampled with the black-box function
-        - Y_init is a numpy.ndarray of shape (t, 1)
-          representing black-box function's outputs for each input in X_init
-          * t is the number of initial samples
-        - bounds is a tuple of (min, max) representing the bounds
-          of the space in which to look for the optimal point
-        - ac_samples is the number of samples that
-          should be analyzed during acquisition
-        - l is the length parameter for the kernel
-        - sigma_f is the standard deviation given to the output
-          of the black-box function
-        - xsi is the exploration-exploitation factor for acquisition
-        - minimize is a bool determining whether optimization should
-          be performed for minimization (True) or maximization (False)
-        - Sets the following public instance attributes:
-          * f: the black-box function
-          * gp: an instance of the class GaussianProcess
-          * X_s: a numpy.ndarray of shape (ac_samples, 1) containing all
-            acquisition sample points, evenly spaced between min and max
-          * xsi: the exploration-exploitation factor
-          * minimize: a bool for minimization versus maximization
-        """
+    '''
+    Class that performs bayesian optimization
+    '''
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
+                 sigma_f=1, xsi=0.01, minimize=True):
+        '''
+        Function init for bayesian optimization
+        '''
         self.f = f
 
         self.gp = GP(X_init, Y_init, l, sigma_f)
 
-        low, high = bounds
-        X_s = np.linspace(start=low, stop=high, num=ac_samples)
+        X_s = np.linspace(bounds[0], bounds[1], num=ac_samples)
         self.X_s = X_s.reshape(-1, 1)
 
         self.xsi = xsi
@@ -52,15 +28,9 @@ class BayesianOptimization:
         self.minimize = minimize
 
     def acquisition(self):
-        """
-        - calculates the next best sample location:
-        - Uses the Expected Improvement acquisition function
-        Returns: X_next, EI
-        - X_next is a numpy.ndarray of shape (1,)
-          representing the next best sample point
-        - EI is a numpy.ndarray of shape (ac_samples,)
-          containing the expected improvement of each potential sample
-        """
+        '''
+        Function that calculates the next best sample location
+        '''
         mu, sigma = self.gp.predict(self.X_s)
 
         if self.minimize is True:
@@ -70,15 +40,15 @@ class BayesianOptimization:
             Y_sample = np.max(self.gp.Y)
             im = mu - Y_sample - self.xsi
 
-        N = sigma.shape[0]
-        Z = np.zeros(N)
-        for i in range(N):
+        ss = sigma.shape[0]
+        Z = np.zeros(ss)
+        for i in range(ss):
             if sigma[i] > 0:
                 Z[i] = im[i] / sigma[i]
             else:
                 Z[i] = 0
-            EI = im * norm.cdf(Z) + sigma * norm.pdf(Z)
+            expo = im * norm.cdf(Z) + sigma * norm.pdf(Z)
 
-        X_next = self.X_s[np.argmax(EI)]
+        X_next = self.X_s[np.argmax(expo)]
 
-        return X_next, EI
+        return X_next, expo
