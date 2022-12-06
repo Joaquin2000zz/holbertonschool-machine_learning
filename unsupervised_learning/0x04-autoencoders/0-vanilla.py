a59b18f877240943cc46411e5082945a9f30707e
Joaquin Victoria <3942@holbertonstudents.com>
2022-12-06 01:40:42 -0300

    exercise 0 working properly

---
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+module which contains autoencoder function
+"""
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """
+     creates an autoencoder:
+
+    - input_dims is an integer containing the dimensions of the model input
+    - hidden_layers is a list containing the number of nodes for each hidden
+      layer in the encoder, respectively
+      * the hidden layers should be reversed for the decoder
+    - latent_dims is an integer containing the dimensions of
+      the latent space representation
+    Returns: encoder, decoder, auto
+      * encoder is the encoder model
+      * decoder is the decoder model
+      * auto is the full autoencoder model
+    - The autoencoder model should be compiled using adam optimization
+      and binary cross-entropy loss
+    - All layers should use a relu activation except for the last layer
+      in the decoder, which should use sigmoid
+    """
+
+    X = keras.Input(shape=(input_dims,))
+    Y = X
+    for l in hidden_layers:
+        Y = keras.layers.Dense(units=l, activation='relu')(Y)
+
+    # bottle neck
+    Y = keras.layers.Dense(units=latent_dims, activation='relu')(Y)
+
+    encoder = keras.Model(X, Y)
+
+    xD = Y
+    for l in reversed(hidden_layers):
+        Y = keras.layers.Dense(units=l, activation='relu')(Y)
+    Y = keras.layers.Dense(units=input_dims, activation='sigmoid')(Y)
+
+    decoder = keras.Model(xD, Y)
+
+    auto = keras.Model(X, Y)
+
+    auto.compile(optimizer='adam', loss='binary_crossentropy',
+                 metrics=['accuracy'])
+
+    return encoder, decoder, auto
