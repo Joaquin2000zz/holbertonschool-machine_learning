34f5f7cc65a581fd553fbd1c1fe06e63b8ad8323
Joaquin Victoria <3942@holbertonstudents.com>
2023-01-10 12:43:47 -0300

    changed get_feature_names_out by get_feature_names

--- supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
+++ supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
@@ -20,4 +20,4 @@ def bag_of_words(sentences, vocab=None):
     """
     vectorizer = CountVectorizer(vocabulary=vocab)
     X = vectorizer.fit_transform(sentences)
-    return X.toarray(), vectorizer.get_feature_names_out()
+    return X.toarray(), vectorizer.get_feature_names()
