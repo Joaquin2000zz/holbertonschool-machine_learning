b710bd4ccd9be54b08dde08d9501355478ad6e9a
Joaquin Victoria <3942@holbertonstudents.com>
2023-01-15 21:04:22 -0300

    working properly

---
+++ supervised_learning/0x10-nlp_metrics/1-ngram_bleu.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+module which contains ngram_bleu function
+"""
+import numpy as np
+
+def n_gram(sentence, n):
+    """
+    generates ngram segmentation 
+    """
+    ngram = []
+    m = len(sentence)
+    for i in range(m - 1):
+        print(i + n)
+        new = ' '.join(sentence[i: i + n])
+        if new not in ngram:
+            ngram.append(new)
+    return ngram
+
+def ngram_bleu(references, sentence, n):
+    """
+    calculates the n-gram BLEU score for a sentence:
+
+    - references: is a list of reference translations
+      * each reference translation is a list of the words in the translation
+    - sentence: is a list containing the model proposed sentence
+    - n: size of the n-gram to use for evaluation
+    Returns: the n-gram BLEU score
+    """
+    count_clip = 0
+    count = 0
+    m = len(sentence)
+    ngram = n_gram(sentence, n)
+    gram_refs = [n_gram(ref, n) for ref in references]
+    print(ngram)
+    for word in ngram:
+        count_clip += np.max([ref.count(word) for ref in gram_refs])
+        count += ngram.count(word)
+
+    r = len(references[np.argmin([abs(len(x) - m) for x in references])])
+    if m > r:
+        brevity_penalty = 1
+    else:
+        brevity_penalty = np.exp(1 - (r / m))
+    return brevity_penalty * (count_clip / count)
