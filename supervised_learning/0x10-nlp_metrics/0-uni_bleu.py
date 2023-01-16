b4bb27ed619b570fd500d7cb26b5fb29ebefad11
Joaquin Victoria <3942@holbertonstudents.com>
2023-01-15 21:24:37 -0300

    didn't change

--- supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -1,45 +1,30 @@
 #!/usr/bin/env python3
 """
-module which contains ngram_bleu function
+module which contains uni_bleu function
 """
 import numpy as np
 
 
-def n_gram(sentence, n=1):
-    """
-    generates ngram segmentation
-    """
-    ngram = []
-    m = len(sentence)
-    for i in range(m):
-        new = ' '.join(sentence[i: i + n])
-        if new not in ngram:
-            ngram.append(new)
-    return ngram
-
-
 def uni_bleu(references, sentence):
     """
-    calculates the n-gram BLEU score for a sentence:
+    calculates the unigram BLEU score for a sentence:
 
     - references: is a list of reference translations
       * each reference translation is a list of the words in the translation
     - sentence: is a list containing the model proposed sentence
-    - n: size of the n-gram to use for evaluation
-    Returns: the n-gram BLEU score
+    Returns: the unigram BLEU score
     """
     count_clip = 0
     count = 0
-    m = len(sentence)
-    ngram = n_gram(sentence)
-    gram_refs = [n_gram(ref) for ref in references]
-    for word in ngram:
-        count_clip += np.max([ref.count(word) for ref in gram_refs])
-        count += ngram.count(word)
+    n = 0
+    for word in sentence:
+        count_clip += np.max([ref.count(word) for ref in references])
+        count += sentence.count(word)
+        n += 1
 
-    r = len(references[np.argmin([abs(len(x) - m) for x in references])])
-    if m > r:
+    r = len(references[np.argmin([abs(len(x) - n) for x in references])])
+    if n > r:
         brevity_penalty = 1
     else:
-        brevity_penalty = np.exp(1 - (r / m))
+        brevity_penalty = np.exp(1 - (r / n))
     return brevity_penalty * (count_clip / count)
