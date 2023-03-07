36be45de74cfaa7ae4310d2864b34382f7d5e653
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-03-07 18:41:40 -0300

    now the exercise 12 it's working properly

--- pipeline/0x00-pandas/12-hierarchy.py
+++ pipeline/0x00-pandas/12-hierarchy.py
@@ -9,9 +9,7 @@ df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
 df1.index = df1.pop('Timestamp')
 df2.index = df2.pop('Timestamp')
 df = pd.concat([df2.loc['1417411980':'1417417981'], df1.loc['1417411980':'1417417981']],
-               keys=['bitstamp', 'coinbase'])
-df.sort_values(by='Timestamp',
-               ascending=True,
-               inplace=True)
+               keys=['bitstamp', 'coinbase']).reorder_levels([1, 0], axis=0)
+df.sort_index(inplace=True)
 
 print(df)
