c443f84ff81500b34708019d032b7ae852e74f5a
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-03-13 09:31:07 -0300

    changed url

--- pipeline/0x01-apis/0-passengers.py
+++ pipeline/0x01-apis/0-passengers.py
@@ -12,27 +12,26 @@ def availableShips(passengerCount):
     """
     if not isinstance(passengerCount, int):
         return []
-    url = 'https://swapi-api.hbtn.io/api/starships/?passengers='
-    url += str(passengerCount)
+    url = 'https://swapi-api.hbtn.io/api/starships/'
     response = requests.get(url)
     if not isinstance(response, requests.Response):
         return []
     if response.status_code != 200:
         return []
 
-    D = response.json()
-    r = D.get('results')
+    d = response.json()
+    r = d.get('results')
     f = [None, 'n/a', 'unknown'] + [str(x) for x in range(0, passengerCount)]
     n, p = 'name', 'passengers'
     listShips = [x.get(n) for x in r if x.get(n) and x.get(p) not in f]
-    D = response.json()
-    next = D.get('next')
+    d = response.json()
+    next = d.get('next')
 
     while response and isinstance(listShips, list) and next:
         response = requests.get(next)
-        D = response.json()
-        r = D.get('results')
+        d = response.json()
+        r = d.get('results')
         listShips += [x.get(n) for x in r if x.get(n) and x.get(p) not in f]
-        next = D.get('next')
+        next = d.get('next')
 
     return listShips
