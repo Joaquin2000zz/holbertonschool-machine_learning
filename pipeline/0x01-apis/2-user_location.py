89871a86c012e84f58c10cd78a8f333e76535fec
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-03-13 15:55:13 -0300

    first attempt

---
+++ pipeline/0x01-apis/2-user_location.py
@@ -0,0 +1,29 @@
+"""
+module which prints the location of a specific user
+"""
+import json as js
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Not Found')
+        exit()
+    arg = sys.argv[1]
+    if not arg:
+        print('Not Found')
+        exit()
+    response = requests.get(arg)
+    if not response:
+        print('Not Found')
+        exit()
+    if response.status_code != 200:
+        if response.status_code == 403:
+            print('Reset in X min')
+            exit()
+    parsed = js.loads(response.text)
+    if not parsed:
+        print('Not Found')
+        exit()
+    print(parsed.get('location'))
