62e690d73bbed0986ade23fdddb65d53a2e16f1e
Joaquin Victoria <3942@holbertonstudents.com>
2023-03-13 19:13:36 -0300

    handled if response is a list

--- pipeline/0x01-apis/3-upcoming.py
+++ pipeline/0x01-apis/3-upcoming.py
@@ -4,7 +4,7 @@ displays the lastest launch information from unofficial SpaceX API
 format:
 <launch name> (<date>) <rocket name> - <launchpad name> (<launchpad locality>)
 """
-
+from datetime import datetime
 import requests
 
 
@@ -12,16 +12,32 @@ if __name__ == '__main__':
     url_base = 'https://api.spacexdata.com/v4/'
     response = requests.get(url_base + 'launches/latest')
     if response.status_code != 200:
-        print('entre al if 0')
         exit()
     r_json = response.json()
-    launch_name, date = r_json.get('name'), r_json.get('date_utc')
+    if isinstance(r_json, list):
+        choose = []
+        indexes = {}
+        for i, elem in enumerate(r_json):
+            date = r_json.get('date_unix')
+            date = datetime.utcfromtimestamp(date).strftime(
+                '%Y-%m-%dT%H:%M:%S.%f%z'
+                )
+            indexes[date] = i
+            choose.append(date)
+            choose.sort()
+        date = choose[0]
+        r_json = r_json[indexes.get(date)]
+
+
+
+    
+    launch_name, date = r_json.get('name'), r_json.get('date_unix')
     if not launch_name or not date:
-        print('entre al if 1')
         exit()
+
+    date = datetime.utcfromtimestamp(date).strftime('%Y-%m-%dT%H:%M:%S.%f%z')
     rocket_id, launchpad_id = r_json.get('rocket'), r_json.get('launchpad')
     if not rocket_id or not launchpad_id:
-        print('entre al if 2')
         exit()
     rocket = requests.get(url_base + 'rockets/' + rocket_id)
     launchpad = requests.get(url_base + 'launchpads/' + launchpad_id)
