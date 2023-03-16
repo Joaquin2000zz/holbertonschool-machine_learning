d1ab3ccc09f8390320810e215c81420a604be78a
Joaquin Victoria <3942@holbertonstudents.com>
2023-03-16 12:30:54 -0300

    using nested ifs instead exit. return does not work outside functions (obviously)

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -9,24 +9,22 @@ import requests
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        return
-    arg = sys.argv[1]
-    if not arg:
         print('Not Found')
-        return
-    response = requests.get(arg)
-    if not response:
-        print('Not Found')
-        return
-    if response.status_code != 200:
+    else:
+        arg = sys.argv[1]
+        if not arg:
+            print('Not Found')
+        
+        response = requests.get(arg)
+        if response.status_code == 200:
+            parsed = response.json()
+            print(
+                parsed.get('location')
+                )
         if response.status_code == 403:
-            X_RateLimit_Reset = response.headers.get('X-RateLimit-Reset')
+            X_RateLimit_Reset = response.headers.get(
+                'X-RateLimit-Reset'
+                )
             now = datetime.now().timestamp()
             distance = (int(X_RateLimit_Reset) - str(now)) / 60
             print('Reset in {distance} min'.format(int(distance)))
-            return
-    parsed = response.json()
-    if not parsed:
-        print('Not Found')
-        return
-    print(parsed.get('location'))
