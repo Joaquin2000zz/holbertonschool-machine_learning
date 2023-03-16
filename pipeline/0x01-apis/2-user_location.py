4f413f3ac75eaa6ed8082258a2d18ef761edc426
Joaquin Victoria <3942@holbertonstudents.com>
2023-03-16 12:33:30 -0300

    fixed documentation

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -14,17 +14,17 @@ if __name__ == '__main__':
         arg = sys.argv[1]
         if not arg:
             print('Not Found')
-        
+
         response = requests.get(arg)
         if response.status_code == 200:
             parsed = response.json()
             print(
                 parsed.get('location')
-                )
+            )
         if response.status_code == 403:
             X_RateLimit_Reset = response.headers.get(
                 'X-RateLimit-Reset'
-                )
+            )
             now = datetime.now().timestamp()
             distance = (int(X_RateLimit_Reset) - str(now)) / 60
             print('Reset in {distance} min'.format(int(distance)))
