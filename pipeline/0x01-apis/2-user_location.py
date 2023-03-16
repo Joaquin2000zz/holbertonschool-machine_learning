c1f744f53c51da08a00de3e5ce5ce2d6e6d078fa
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-03-16 17:24:47 +0000

    again ubuntu 14.04 LTS

---
+++ pipeline/0x01-apis/2-user_location.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+module which prints the location of a specific user
+"""
+from datetime import datetime
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Not Found')
+    else:
+        arg = sys.argv[1]
+        if not arg:
+            print('Not Found')
+
+        response = requests.get(arg)
+        if response.status_code == 404:
+            print('Not Found')
+        if response.status_code == 200:
+            parsed = response.json()
+            print(parsed, parsed)
+            message = parsed.get('message')
+            if message == 'Not Found':
+                print(message)
+            else:
+                print(
+                    parsed.get('location')
+                )
+        if response.status_code == 403:
+            X_RateLimit_Reset = response.headers.get(
+                'X-RateLimit-Reset'
+            )
+            now = datetime.now().timestamp()
+            distance = (int(X_RateLimit_Reset) - str(now)) / 60
+            print('Reset in {distance} min'.format(int(distance)))
