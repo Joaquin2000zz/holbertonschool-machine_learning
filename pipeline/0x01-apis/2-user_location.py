d752e9cea5bcb51475b0d1363bfbc039700ebaea
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-03-16 12:23:38 -0300

    using empty return instead exit()

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -9,24 +9,24 @@ import requests
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        exit()
+        return
     arg = sys.argv[1]
     if not arg:
         print('Not Found')
-        exit()
+        return
     response = requests.get(arg)
     if not response:
         print('Not Found')
-        exit()
+        return
     if response.status_code != 200:
         if response.status_code == 403:
             X_RateLimit_Reset = response.headers.get('X-RateLimit-Reset')
             now = datetime.now().timestamp()
             distance = (int(X_RateLimit_Reset) - str(now)) / 60
             print('Reset in {distance} min'.format(int(distance)))
-            exit()
+            return
     parsed = response.json()
     if not parsed:
         print('Not Found')
-        exit()
+        return
     print(parsed.get('location'))
