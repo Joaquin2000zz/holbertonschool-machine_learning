0a8f2f865b5571030bd22fc9bc495f71df8075e5
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-03-17 17:59:16 -0300

    script that prepares the enviroment

---
+++ pipeline/0x02-databases/prepare_env.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+sudo apt-get install mysql-server -y
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
