f38661d7ea776b04c9d781ce1305afea129ea6f8
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-03-18 17:54:01 -0300

    fixed doc???

--- pipeline/0x02-databases/0-create_database_if_missing.sql
+++ pipeline/0x02-databases/0-create_database_if_missing.sql
@@ -1,3 +1,3 @@
- -- create_database_if_missing - creates the database db_0 in a MySQL server
+ -- create_database_if_missing, creates the database db_0 in a MySQL server
  -- You are not allowed to use the SELECT or SHOW statements
 CREATE DATABASE IF NOT EXISTS `db_0`;
