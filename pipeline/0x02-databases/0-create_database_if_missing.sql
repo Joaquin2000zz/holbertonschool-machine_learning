af341cc8f53ba14fd4d66e5d1a92cc36dbfc9f7c
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-03-18 16:45:25 -0300

    deleted semicolon

--- pipeline/0x02-databases/0-create_database_if_missing.sql
+++ pipeline/0x02-databases/0-create_database_if_missing.sql
@@ -1,4 +1,4 @@
  -- create_database_if_missing - creates the database db_0 in a MySQL server
  -- * If the database db_0 already exists, your script should not fail
  -- * You are not allowed to use the SELECT or SHOW statements
-CREATE DATABASE IF NOT EXISTS db_0;
+CREATE DATABASE IF NOT EXISTS db_0
