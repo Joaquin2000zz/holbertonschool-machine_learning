a84dc21d5abea9afe306dfe89170b3a2ccbbbfc6
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-03-19 13:58:14 -0300

    erased all error checking

--- pipeline/0x01-apis/3-upcoming.py
+++ pipeline/0x01-apis/3-upcoming.py
@@ -10,8 +10,6 @@ import requests
 if __name__ == '__main__':
     url_base = 'https://api.spacexdata.com/v4/'
     response = requests.get(url_base + 'launches/upcoming')
-    if response.status_code != 200:
-        exit()
     r_json = response.json()
     response_sorted = sorted(r_json,
                              key=lambda x: x.get('date_unix'),
@@ -20,19 +18,13 @@ if __name__ == '__main__':
 
     date = choose.get('date_local')
     launch_name = choose.get('name')
-    if not launch_name or not date:
-        exit()
 
     rocket_id, launchpad_id = choose.get('rocket'), choose.get('launchpad')
-    if not rocket_id or not launchpad_id:
-        exit()
+    dateu = choose.get('date_unix')
+
     rocket = requests.get(url_base + 'rockets/' + rocket_id)
     launchpad = requests.get(url_base + 'launchpads/' + launchpad_id)
-    if rocket.status_code != 200 or launchpad.status_code != 200:
-        exit()
     rocket, launchpad = rocket.json(), launchpad.json()
-    if not rocket or not launchpad:
-        exit()
     rocket_name, launchpad_name = rocket.get('name'), launchpad.get('name')
     launchpad_locality = launchpad.get('locality')
 
