0cb1bdd087f83865f1fa2ef0b2458967e83d85b2
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-04-03 18:26:51 -0300

    now it's working properly

--- pipeline/0x04-data_augmentation/100-pca.py
+++ pipeline/0x04-data_augmentation/100-pca.py
@@ -1,31 +1,30 @@
 def pca_color(image, alphas=None):
-    """
-    performs PCA color augmentation as described in the AlexNet paper:
-
-    - image: is a 3D tf.Tensor containing the image to change
-    - alphas: a tuple of length 3 containing the amount that
-    each channel should change
-    Returns: the augmented image
-    """
     # convert the image tensor to a 2D tensor
     h, w, c = image.shape
-    flattened_image = tf.reshape(image, [-1, c])
+    flattened_image = tf.cast(tf.reshape(image, [-1, c]),
+                              tf.float32)
     # subtract the mean from each pixel
-    mean = tf.reduce_mean(flattened_image, axis=0)
-    centered_image = tf.cast(image - mean, tf.float32) 
+    mean = tf.math.reduce_mean(flattened_image, axis=0)
+    centered_image = flattened_image - mean
+    stdd = tf.math.reduce_std(centered_image, axis=0)
+    centered_image /= stdd
     # compute covariance matrix
     covariance = tf.matmul(centered_image, centered_image,
-                           transpose_a=True) / tf.cast(h * w, tf.float32)
+                           transpose_a=True) / tf.cast(h * w, tf.float32) - 1
     # compute the eigenvectors and eigenvalues of the covariance matrix
     eigenvalues, eigenvectors = tf.linalg.eigh(covariance)
-    # sort the eigenvectors by descending eigenvalues
-    sorted_indices = tf.argsort(eigenvalues,
-                                direction='DESCENDING')
-    sorted_eigenvectors = tf.gather(eigenvectors, sorted_indices, axis=1)
-    # compute the projection matrix and project the centered image onto it
-    projection_matrix = sorted_eigenvectors[:, :c]
-    if None not in alphas:
-      projection_matrix = tf.multiply(projection_matrix, alphas) 
-    pca_image = tf.matmul(centered_image, projection_matrix)
+
+    alphas = alphas * eigenvalues
+    projection_matrix = tf.matmul(eigenvectors, alphas[:, tf.newaxis])
+      
+    pca_image = centered_image + tf.transpose(projection_matrix)
+    pca_image *= stdd
+    pca_image += mean
+    # comment this line if you want cool images
+    pca_image = tf.math.maximum(tf.math.minimum(pca_image, 255)
+                                , 0)
+    pca_image = tf.cast(pca_image, dtype='uint8')
     pca_image = tf.reshape(pca_image, [h, w, c])
+
+
     return pca_image
